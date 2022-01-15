55d327fbf93cc91e5ab2980e62ed2fd6e5c10b5a
pride829 <pride829@gmail.com>
2022-01-15 23:17:24 +0800

    Reconstruct. Rename the main file to "shioaji coverer.\nAdd method stop_price_updater and future_code."

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to Shiaoji Coverer
+
+Enviorment requirement:
+* Python 3.7
+* Shioaji
+
+Enviorment setup:
+
+First install python 3.7 on your computer.
+Then run ```pip install shioaji``` on the cmd.
+
+Usage:
+
+Don't use "Auto" when placing an order.
--- shioaji_login.py
+++ shioaji_login.py
@@ -1,21 +1,36 @@
-def shioaji_login():
-    import os
-    from pathlib import Path
-    ca_path = os.path.abspath("./Sinopac.pfx")
-    import json
-    import shioaji as sj
+import json
+import threading
+import os
+import shioaji as sj
+
+from pathlib import Path
+
+def login():
+    """
+    Login with account info specify in the file 'account_info.json'.
+    :return: shioaji api.
+    """
+
     account_info = 'account/account_info.json'
+    
     with open(account_info, newline='') as jsonfile:
         account_data = json.load(jsonfile)
+        
+    ca_path = account_data['ca_path']
+    ca_name = account_data['ca_name']
+    ca_path = ca_path + ca_name
+    ca_path = os.path.abspath(ca_path)
+
     api = sj.Shioaji(simulation=False) 
-    print(f'正在使用{account_data["person_id"]}的ID登入...')
+    
     api.login(
         person_id = account_data['person_id'],
         passwd = account_data['passwd'], 
-        contracts_cb=lambda security_type: print(f"{repr(security_type)} fetch done.")
+        #contracts_cb=lambda security_type: print(f"{repr(security_type)} fetch done.")
     )
+    print(f'Login with ID {account_data["person_id"]}')
     
-    print(f'正在啟用位於{ca_path}的憑證...')
     activate = api.activate_ca(ca_path=ca_path, ca_passwd=account_data['ca_passwd'], person_id=account_data['person_id'])
+    print(f'Activating CA at the path {ca_path}')
     
     return api
