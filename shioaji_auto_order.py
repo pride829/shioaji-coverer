ef843b1272a38d52a099e4d914db383aa2430cad
pride829 <pride829@gmail.com>
2022-04-18 17:59:39 +0800

    Feature: Minor changes to auto_order. Update auto order document.

--- shioaji_auto_order.py
+++ shioaji_auto_order.py
@@ -26,6 +26,17 @@ importlib.reload(shioaji_login)
 # In[2]:
 
 
+def report_time():
+    global NQ_price
+    
+    while True:
+        print("Time report:", datetime.datetime.now().strftime("%H:%M:%S"), ", price:", NQ_price)
+        time.sleep(auto_order_time_report)
+
+
+# In[3]:
+
+
 def write_log(text):
     """
     Write into log file.
@@ -53,7 +64,60 @@ def write_log(text):
     
 
 
-# In[3]:
+# In[4]:
+
+
+def list_positions():
+    """
+    List all positions.
+    :global param: positions
+    """
+    
+    print('The position currently tracking:')
+
+    if not positions:
+        print('Empty.')
+
+    for p in positions:
+
+        if(p[0] == 1):
+            action_text = "Long"
+        else:
+            action_text = "Short"
+
+        print(f'[Type: {action_text}, quantity: {p[1]}, deal price: {p[2]}, best price: {p[3]}, cover order had been sent: {p[4]}]')
+    
+
+
+# In[5]:
+
+
+def send_test_msg(
+    price,
+    quantity,
+    action,
+    stat=sj.constant.OrderState.FDeal,
+    code='MXF',
+    delivery_month='202201',
+    security_type='FUT'
+):
+    """
+    For test purpose.
+    """
+    # Testing with msg
+
+    msg = {}
+    msg['price'] = price
+    msg['quantity'] = quantity
+    msg['action'] = action
+    msg['code'] = code
+    msg['delivery_month'] = delivery_month
+    msg["security_type"] = security_type
+
+    place_cb(stat, msg)
+
+
+# In[6]:
 
 
 def place_order(quantity, action):
@@ -64,7 +128,19 @@ def place_order(quantity, action):
     :return: None
     """
     
-    global auto_order_testing
+    global auto_order_testing, positions
+    
+    if positions:
+        print('***')
+        log_msg = f'A order with action={action}, quantity={quantity} should be place, but there is still position in hold, so the order will not be sent.'
+        
+        print(log_msg)
+        write_log(log_msg)
+        
+        list_positions()
+        print('***')
+        time.sleep(10)
+        return
     
     if not auto_order_testing:
         fut_order = api.Order(
@@ -93,9 +169,11 @@ def place_order(quantity, action):
         print(log_msg)
         write_log(log_msg)
         print('***\n')
+        
+    time.sleep(10)
 
 
-# In[4]:
+# In[7]:
 
 
 def get_future_code(future_name):
@@ -136,14 +214,19 @@ def get_future_code(future_name):
     return future_code
 
 
-# In[5]:
+# In[8]:
 
 
 def update_config():
     
-    global future_name, future_code, auto_order_time, auto_buy_trigger, auto_sell_trigger, auto_order_testing
+    global future_name, future_code
+    global auto_order_consec_tick, auto_order_time, auto_buy_trigger, auto_sell_trigger, auto_order_testing
+    global auto_order_time_report
     
-    pre_future_code = pre_auto_order_time = pre_auto_buy_trigger = pre_auto_sell_trigger = pre_auto_order_testing = None
+    pre_future_code = pre_auto_order_time = None
+    pre_auto_buy_trigger = pre_auto_sell_trigger = pre_auto_order_testing = None
+    pre_auto_order_consec_tick = pre_auto_order_time_report = None
+    
     
     
     while(True):
@@ -152,11 +235,16 @@ def update_config():
             config_data = json.load(f)
             
             future_name = config_data['future_name']
-            auto_order_time = int(config_data['auto_order_time'])
+            auto_order_time = float(config_data['auto_order_time'])
             auto_buy_trigger = float(config_data['auto_buy_trigger'])
             auto_sell_trigger = float(config_data['auto_sell_trigger'])
-            auto_order_testing = bool(config_data['auto_order_testing'])
-        
+            if config_data['auto_order_testing'] == "False":
+                auto_order_testing = False
+            else:
+                auto_order_testing = True
+            auto_order_consec_tick = int(config_data['auto_order_consec_tick'])
+            auto_order_time_report = float(config_data['auto_order_time_report'])
+            
             # If auto_recent_future, get the most recent future code.
             if(config_data['auto_recent_future'].lower() == 'true'):
                 future_code = get_future_code(future_name)
@@ -171,6 +259,10 @@ def update_config():
                 print(f'Auto order time has been set to {auto_order_time}')
                 pre_auto_order_time = auto_order_time
             
+            if(pre_auto_order_consec_tick != auto_order_consec_tick):
+                print(f'Auto consecutive tick has been set to {auto_order_consec_tick}')
+                pre_auto_order_consec_tick = auto_order_consec_tick
+            
             if(pre_auto_buy_trigger != auto_buy_trigger):
                 print(f'Auto buy trigger has been set to {auto_buy_trigger}')
                 pre_auto_buy_trigger = auto_buy_trigger
@@ -183,40 +275,95 @@ def update_config():
                 print(f'Auto order testing has been set to {auto_order_testing}')
                 pre_auto_order_testing = auto_order_testing
                 
+            if(pre_auto_order_time_report != auto_order_time_report):
+                print(f'Auto order time report period has been set to {auto_order_time_report}')
+                pre_auto_order_time_report = auto_order_time_report    
+                
             time.sleep(1)
 
 
-# In[6]:
+# In[9]:
 
 
+# 在這裡下單
 def OnRealTimeQuote(symbol):
-    global NQ_price, price_history
+    
+    global NQ_price, price_history, trade_lock
+    
+    #print("OnRealTimeQuote: " + str(NQ_price))
+    
+    # price_history(不含最新的一個price)的長度即為中間間隔幾個tick的長度
+    while len(price_history) > auto_order_consec_tick:
+        del price_history[0]
+    
     NQ_price = float(symbol['TradingPrice'])
+    if NQ_price == 0:
+        return
+    
+    
     price_history.append([NQ_price, datetime.datetime.now()])
-    if NQ_price - price_history[0][0] > auto_buy_trigger:
-        msg_log = "A huge increasing in price has been detected!\n"
-        msg_log += "Price " + str((datetime.datetime.now() - price_history[0][1]).total_seconds()) + " seconds ago: " + str(price_history[0][0]) + "\n"
-        msg_log += "Price now: " + str(NQ_price)
-        write_log(msg_log)
-        print(msg_log)
-        place_order(1, sj.constant.Action.Buy)
-        price_history = []
-    elif price_history[0][0] - NQ_price > auto_sell_trigger:
-        msg_log = "A huge decreasing in price has been detected!\n"
-        msg_log += "Price " + str((datetime.datetime.now() - price_history[0][1]).total_seconds()) + " seconds ago: " + str(price_history[0][0]) + "\n"
-        msg_log += "Price now: " + str(NQ_price)
-        write_log(msg_log)
-        print(msg_log)
-        place_order(1, sj.constant.Action.Sell)
-        price_history = []
-    else:
-        while (datetime.datetime.now() - price_history[0][1]).total_seconds() > auto_order_time:
-            # Delete data that is too old
-            del price_history[0]
-            
+    
+    if len(price_history) < 2:
+        return
+    
+    pre_price_diff = 0
+    
+    if trade_lock: # trade_lock=True時仍然會記錄價格，但不會判斷是否要trade
+        return
+    
+    for i in range(len(price_history) - 1, 0, -1): # 從倒數第二個traverse到第一個
+        
+        price_diff = price_history[i][0] - price_history[i-1][0]
+        
+        
+        #print(pre_price_diff, price_diff)
+        if pre_price_diff > 0 and pre_price_diff <= price_diff:
+            return
+        elif pre_price_diff < 0 and pre_price_diff >= price_diff:
+            return
+        
+        pre_price_diff = price_diff
+        
+        tick_diff = price_history[-1][0] - price_history[i-1][0]
+        
+        if (price_history[-1][1] - price_history[i-1][1]).total_seconds() > auto_order_time:
+            # 超時
+            break
+        
+
+        
+        if price_diff > 0 and         tick_diff > auto_buy_trigger:
+            msg_log = "A huge increasing in price has been detected!\n"
+            msg_log += "NQ_Price " + str((price_history[-1][1] - price_history[i-1][1]).total_seconds()) +             " seconds ago: \n" + str(price_history[i-1][0]) +             ", " + str(price_history[i-1][1]) +             "\n"
+            msg_log += "Ticks in between:\n"
+            for j in range(i, len(price_history)-1):
+                msg_log += str(price_history[j][0]) + ", " + str(price_history[j][1]) + "\n"
+            msg_log += "NQ_Price now: \n" + str(price_history[-1][0]) + ", " + str(price_history[-1][1]) + "\n"
+            write_log(msg_log)
+            print(msg_log)
+            trade_lock = True
+            place_order(1, sj.constant.Action.Buy)
+            price_history = []
+            trade_lock = False
+            return
+        
+        if price_diff < 0 and         tick_diff < -auto_sell_trigger:
+            msg_log = "A huge decreasing in price has been detected!\n"
+            msg_log += "NQ_Price " + str((price_history[-1][1] - price_history[i-1][1]).total_seconds()) +             " seconds ago: \n" + str(price_history[i-1][0]) +             ", " + str(price_history[i-1][1]) +             "\n"
+            msg_log += "Ticks in between:\n"
+            for j in range(i, len(price_history)-1):
+                msg_log += str(price_history[j][0]) + ", " + str(price_history[j][1]) + "\n"
+            msg_log += "NQ_Price now: \n" + str(price_history[-1][0]) + ", " + str(price_history[-1][1]) + "\n"
+            write_log(msg_log)
+            print(msg_log)
+            trade_lock = True
+            place_order(1, sj.constant.Action.Sell)
+            price_history = []
+            trade_lock = False
+            return
 
 
-# In[7]:
+# In[10]:
 
 
 def quote_sub_th(obj,sub_port,filter = ""):
@@ -252,14 +399,119 @@ def quote_sub_th(obj,sub_port,filter = ""):
     return
 
 
-# In[8]:
+# In[11]:
+
+
+def fill_positions(deal):
+    """
+    :global param positions: (list)
+    
+    :return: None//    """
+
+    global positions
+    # First check if the type and month match the tracking future.
+    if(
+        deal['code'] != contract['category'] or
+        deal['delivery_month'] != contract['delivery_month'] or
+        deal["security_type"] != 'FUT'
+      ):
+        print("This deal is not as same as the future currently tracking.")
+        return
+    
+    price = int(deal['price'])
+    quantity = int(deal['quantity'])
+    try:
+        if(deal['action'] == 'Buy'):
+            action = 1
+        elif(deal['action'] == 'Sell'):
+            action = -1
+        else:
+            raise ValueError('The action of this deal is neither "Buy" or "Sell".')
+    except ValueError as err:
+        traceback.print_exc()
+    
+    if(action == 1):
+        action_text = "Long"
+    else:
+        action_text = "Short"
+    
+    # While there are still some positions and it is the oppsite of the deal:
+    ori_quantity = quantity
+    while(positions and positions[0][0] == -action and quantity > 0):
+        
+        if(positions[0][1] > quantity):
+            positions[0][1] -= quantity
+            quantity = 0
+            # The deal has been recorded, exit the function
+            break
+        else:
+            quantity -= positions[0][1]
+            del positions[0]
+    print('***')
+    log_msg = f'A position with type={action_text}, quantity={ori_quantity}, price={price} has been recorded!'
+    print(log_msg)
+    write_log(log_msg)
+    print('***\n')
+    
+    if (quantity > 0):
+        
+        # Ensure the data type is int
+        
+        
+        if(action == 1):
+            positions.append([action, int(quantity), int(price), int(price), False])
+            positions = sorted(positions, key=lambda p: p[2], reverse=False)
+        else:
+            positions.append([action, int(quantity), int(price), int(price), False])
+            positions = sorted(positions, key=lambda p: p[2], reverse=True)
+
+        print('***')
+        log_msg = f'A position with type={action_text}, quantity={quantity}, price={price} has been added to the track list!'
+        print(log_msg)
+        write_log(log_msg)
+        print('***\n')
+
+
+    list_positions()
+
+# In[12]:
+
+
+msg_list = []
+
+def place_cb(stat, msg):
+    """
+    Called every time an order or a deal has been detected.
+    
+    :global param: msg_list ()
+    """
+    
+    global msg_list
+    
+    if(stat == sj.constant.OrderState.FOrder):
+        print('An order has been detected.')
+        print(f'op_msg: \"{msg["operation"]["op_msg"]}\"')
+        write_log('stat: ' + stat + '\nmsg: ' + json.dumps(msg, ensure_ascii=False) )
+        msg_list.append(msg)
+    elif(stat == sj.constant.OrderState.FDeal):
+        print('A deal has been detected.')
+        print(f'Deal information: code:{msg["code"]}, action:{msg["action"]}, price:{msg["price"]}, quantity:{msg["quantity"]}')
+        print(f'Delivery month:{msg["delivery_month"]}, security type: {msg["security_type"]}')
+        write_log('stat: ' + stat + '\nmsg: ' + json.dumps(msg, ensure_ascii=False) )
+        msg_list.append(msg)
+        fill_positions(msg)
+
+
+# In[13]:
 
 
 future_name = future_code = None
 
 auto_order_time = auto_buy_trigger = auto_sell_trigger = None
 
-auto_order_testing = None
+auto_order_testing = auto_order_time_report = None
+
+trade_lock = False
 
 update_config_thread = threading.Thread(target = update_config)
 update_config_thread.start()
@@ -269,14 +521,19 @@ time.sleep(3)
 api = shioaji_login.login()
 
 contract = api.Contracts.Futures[future_code]
+
+api.set_order_callback(place_cb)
+
 try:
     if(not contract):
         raise ValueError(f'Error: contract {future_code} does not exsits.')
 except ValueError as err:
     traceback.print_exc()
+    
+positions = []
 
 
-# In[9]:
+# In[14]:
 
 
 NQ_price = 0
@@ -300,7 +557,7 @@ g_QuoteSession = q_data["SessionKey"]
 #查詢指定合约訊息
 quoteSymbol = "TC.F.CME.NQ.HOT"
 
-print("Subscribing to CME.NQ .")
+print("Subscribing to CME.NQ.HOT")
 
 t2 = threading.Thread(target = quote_sub_th,args=(g_QuoteZMQ,q_data["SubPort"],))
 t2.start()
@@ -310,6 +567,6 @@ g_QuoteZMQ.UnsubQuote(g_QuoteSession, quoteSymbol)
 #訂閱實時行情
 g_QuoteZMQ.SubQuote(g_QuoteSession, quoteSymbol)
 
-
-
-
+time.sleep(5)
+report_time_thread = threading.Thread(target = report_time)
+report_time_thread.start()
